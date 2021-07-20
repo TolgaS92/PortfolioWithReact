d1166f97634487711bf94d7577f4fbddd83e3270
Tolga Secme <tolgasecme@icloud.com>
2021-07-19 23:22:04 -0600

    scroll button edit

--- src/components/ScrollUpButton/index.js
+++ src/components/ScrollUpButton/index.js
@@ -1,15 +1,14 @@
 import React from "react";
-import ScrollUpButton from "react-scroll-up-button"; //Add this line Here
-import './style.css';
+import ScrollUpButton from "react-scroll-up-button";
 
 export default class Index extends React.Component {
-    render() {
-        return (
-            <div>
-                <ScrollUpButton
-                AnimationDuration={500}
-                />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <ScrollUpButton
+        AnimationDuration = {250}
+        />
+      </div>
+    );
+  }
 }
