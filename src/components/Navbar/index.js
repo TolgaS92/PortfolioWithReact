1a097c2f0b4c7292a535f1a8f213085a8434f3ed
Tolga Secme <tolgasecme@icloud.com>
2021-08-16 13:02:21 -0600

    deploymentexample

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -13,16 +13,16 @@ function Navbar () {
                     <div className="collapse navbar-collapse" id="navbarsExample007">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item active">
-                                <a className="nav-link" href="/PortfolioWithReact/about">About</a>
+                                <a className="nav-link" href="/about">About</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/PortfolioWithReact/portfolio">Portfolio</a>
+                                <a className="nav-link" href="/portfolio">Portfolio</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/PortfolioWithReact/certificates">Certificates</a>
+                                <a className="nav-link" href="/certificates">Certificates</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/PortfolioWithReact/contact">Contact</a>
+                                <a className="nav-link" href="/contact">Contact</a>
                             </li>
                         </ul>
                     </div>
